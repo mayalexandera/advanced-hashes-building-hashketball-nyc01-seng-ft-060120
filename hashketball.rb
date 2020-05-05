f14acb75a4c0db788b427670a685a11c18314fb4
Maya Blevins <maya.blevins@gmail.com>
2020-05-05 16:52:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -86,17 +86,17 @@ def game_hash
           :assists => 7,
           :steals => 22,
           :blocks => 15,
-          :slam_dunks => 2
+          :slam_dunks => 10
         },
         {
-          :player_name => "Jeff Adrien",
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
+          :player_name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
           :slam_dunks => 2
         },
         {
