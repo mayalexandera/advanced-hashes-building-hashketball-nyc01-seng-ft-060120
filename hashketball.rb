2d55e4f79a9ad48272a0cd9c5aacafc23d1bbff2
Maya Blevins <maya.blevins@gmail.com>
2020-05-05 16:46:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :colors => ["black", "white"],
       :players => [
         {
-          :player_name => "",
+          :player_name => "Alan Anderson",
           :number => "",
           :shoe => "",
           :points => "",
