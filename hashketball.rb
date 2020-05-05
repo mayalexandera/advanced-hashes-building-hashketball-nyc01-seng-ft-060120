81f7f6d784a5532f31b1f94ed4cf51ab596c12ac
Maya Blevins <maya.blevins@gmail.com>
2020-05-05 16:38:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,23 +18,22 @@ def game_hash
         }
       ]
     },
-    :away = {
-      :team_name = "",
-      :colors = [],
-      :players = [
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [
         {
-          :player_name = "",
-          :number = "",
-          :shoe = "",
-          :points = "",
-          :rebounds = "",
-          :assists = "",
-          :steals = "",
-          :blocks = "",
-          :slam_dunks = ""
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => "",
+          :rebounds => "",
+          :assists => "",
+          :steals => "",
+          :blocks => "",
+          :slam_dunks => ""
         }
       ]
-    }
   }
   
 end
